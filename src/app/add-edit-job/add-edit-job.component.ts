4edfee78f1a59176bbd12c26ac324e3765b889a9
Veronica Dinu <veronica@qoffice.ro>
2022-02-09 13:18:04 +0200

    -

--- src/app/add-edit-job/add-edit-job.component.ts
+++ src/app/add-edit-job/add-edit-job.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { JobsService } from '../services/jobs.service';
 
 @Component({
   selector: 'app-add-edit-job',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditJobComponent implements OnInit {
 
-  constructor() { }
+  constructor(private jobsService: JobsService) { }
 
   ngOnInit(): void {
   }
